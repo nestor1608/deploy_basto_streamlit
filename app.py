eaf0daf1e629b3068f728905bc606fb516ade379
nestor1608 <nestor.268@Outlook.com>
2023-04-12 19:34:01 -0300

    update error

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from suport_st import grafic_map,mapbox_access_token
 import plotly.graph_objects as go
 import plotly.express as px
 from funciones_app import setle_lat,setle_lng,area_perimetro,filter_time_day,interview_vaca,data_devices,week_data_filter
-from conect_datarows import setle_clean,selec_setle,count_register_week,obtener_fecha_inicio_fin
+from conect_datarows import setle_clean,selec_setle,obtener_fecha_inicio_fin
 
 
 setle= setle_clean()# arroja dataframe arreglado de setle---
@@ -21,8 +21,9 @@ uuid_devis = on_perimetro.UUID.values
 
 select=st.selectbox("seleccionar collar",uuid_devis)
 dt_vaca=  data_devices(on_perimetro,select)
-
-data_week= count_register_week(dt_vaca)
+data_week= dt_vaca.groupby(['UUID',dt_vaca.createdAt.dt.week]).agg({'createdAt':'count'}).rename(columns={'createdAt':'count_register'})
+data_week=data_week.reset_index()
+print(data_week.columns)
 fig= px.bar( data_week,x='createdAt',y='count_register')
 st.plotly_chart(fig,use_container_width=True) 
 week= st.slider('Selecione semana',int(data_week['createdAt'].min()) ,int(data_week['createdAt'].max()) )
