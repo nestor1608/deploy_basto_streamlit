58ae7b748d2e96ce4f283fcb9ca64f2ec8c25371
nestor1608 <nestor.268@Outlook.com>
2023-04-27 20:10:55 -0300

    resolved-1

--- pages/01 Home.py
+++ pages/01 Home.py
@@ -57,7 +57,7 @@ if on_perimetro.shape[0]!=0:
     dt_vaca=  data_devices(on_perimetro,select)
     dt_vaca.createdAt= pd.to_datetime(dt_vaca.createdAt)
 
-    data_week= dt_vaca.groupby('UUID',dt_vaca.createdAt.dt.week).agg({'createdAt':'count'}).rename(columns={'createdAt':'count_register'})
+    data_week= dt_vaca.groupby(['UUID',dt_vaca.createdAt.dt.week]).agg({'UUID':'count'}).rename(columns={'UUID':'count_register'})
     data_week=data_week.reset_index()
 
 
